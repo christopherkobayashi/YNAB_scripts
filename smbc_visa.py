ab763898b576e3358bbe20a5a7d249df89f30960
Christopher Kobayashi <software+github@disavowed.jp>
2020-06-30 11:15:29 +0900

    Refactor to deal with the two different formats.  Ignore lines that aren't transactions

--- smbc_visa.py
+++ smbc_visa.py
@@ -34,79 +34,77 @@ from datetime import date
 from datetime import datetime
 
 if len(sys.argv) is not 2:
-	print ("smbc_visa.py: must specify an input file.")
-	exit (1)
+    print("smbc_visa.py: must specify an input file.")
+    exit(1)
 
-print ('!Type:CCard')
-
-first_line = 0
+print('!Type:CCard')
 
 ymd_field = 0
 payee_field = 1
 
-with open( sys.argv[1], 'rb' ) as csvfile:
-	rows = csv.reader(csvfile, delimiter=',')
-	for row in rows:
+with open(sys.argv[1], 'rb') as csvfile:
+    rows = csv.reader(csvfile, delimiter=',')
+    for row in rows:
 
-		if not row[0]:
-			continue
-		if row[0] and row[0][0].isdigit() and not first_line:
-			amount_field = 6
-			period_field = 2
-			period_count = 5
-			currency_field = 10
-			amount_foreign = 9
-			amount_ratio = 11
-			comment_field = 4
-			type = 1
-			first_line = 1
-		elif not first_line:
-			amount_field = 5
-			period_count = 3
-			period_field = 4
-			comment_field = 6
-			type = 2
-			first_line = 1
-			continue
+        if len(row) < 7:
+            continue
+        elif len(row) == 7:  # settled statement
+            amount_field = 5
+            period_count = 3
+            period_field = 4
+            comment_field = 6
+            type = 2
+        else:  # unsettled statement
+            amount_field = 6
+            period_field = 2
+            period_count = 5
+            currency_field = 10
+            amount_foreign = 9
+            amount_ratio = 11
+            comment_field = 4
+            type = 1
 
-		year, month, day = row[ymd_field].split('/')
-		trans_date = date(int(year), int(month), int(day))
-		payee = row[payee_field].decode('shift-jis').encode('utf-8')
-		payee = unicodedata.normalize('NFKC', payee.decode('utf8'))
-		payee = re.sub("\s\s+", " ", payee)
-		if type is 1 and row[amount_field+1].isdigit():
-			amount = row[amount_field+1]
-		else:
-			amount = row[amount_field]
+        # if date field is empty, re-use previous date
+        if row[ymd_field]:
+            year, month, day = row[ymd_field].split('/')
+        trans_date = date(int(year), int(month), int(day))
+        payee = row[payee_field].decode('shift-jis').encode('utf-8')
+        payee = unicodedata.normalize('NFKC', payee.decode('utf8'))
+        payee = re.sub("\s\s+", " ", payee)
+        if type is 1 and row[amount_field+1].isdigit():
+            amount = row[amount_field+1]
+        else:
+            amount = row[amount_field]
 
-		if type is 1:
-			# if the charge is made by someone other than you, add it)
-			if row[period_field] != "ご本人" and row[period_field] != 1:
-				comment = row[period_field] + ' '
-			comment = 'Period: ' + row[period_count]
-			if row[currency_field] and row[currency_field] != "JPY":
-				comment = comment + ' ' + row[currency_field] + ' ' + row[amount_ratio] + ' (' + row[amount_foreign] + ')'
-			if row[comment_field]:
-				comment = comment + ' (' + row[comment_field] + ')'
-				newmonth = int(month)+(int(row[comment_field][1])-1)
-				if newmonth > 12:
-					newmonth = newmonth - 12
-					newyear = int(year)+1
-					year = str(newyear)
-				month = str(newmonth)
-				trans_date = date(int(year), int(month), int(day))
-		else:
-				comment = row[comment_field]
+        if type is 1:
+            # if the charge is made by someone other than you, add it)
+            if row[period_field] != "ご本人" and row[period_field] != 1:
+                comment = row[period_field] + ' '
+            comment = 'Period: ' + row[period_count]
+            if row[currency_field] and row[currency_field] != "JPY":
+                comment = comment + ' ' + \
+                    row[currency_field] + ' ' + row[amount_ratio] + \
+                    ' (' + row[amount_foreign] + ')'
+            if row[comment_field]:
+                comment = comment + ' (' + row[comment_field] + ')'
+                newmonth = int(month)+(int(row[comment_field][1])-1)
+                if newmonth > 12:
+                    newmonth = newmonth - 12
+                    newyear = int(year)+1
+                    year = str(newyear)
+                month = str(newmonth)
+                trans_date = date(int(year), int(month), int(day))
+        else:
+            comment = row[comment_field]
 
-		comment = comment.decode('shift-jis').encode('utf-8')
-		comment = unicodedata.normalize('NFKC', comment.decode('utf8'))
-		comment = re.sub("\s\s+", " ", comment)
+        comment = comment.decode('shift-jis').encode('utf-8')
+        comment = unicodedata.normalize('NFKC', comment.decode('utf8'))
+        comment = re.sub("\s\s+", " ", comment)
 
-		amount = -int(amount)
+        amount = -int(amount)
 
-		print ('D' + trans_date.strftime('%Y/%m/%d'))
-		print ('T' + str(amount))
-		print ('P' + payee.encode('UTF-8'))
-		print ('M' + comment.encode('UTF-8'))
-		print ('^')
-
+        print('D' + trans_date.strftime('%Y/%m/%d'))
+        print('T' + str(amount))
+        print('P' + payee.encode('UTF-8'))
+        print('M' + comment.encode('UTF-8'))
+        print('^')
